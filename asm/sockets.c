eb1c9b9ce7ea093645635af7e76bf82e4e4e60a1
mahanfarzaneh2000 <mahan.farzaneh@gmail.com>
2023-10-30 17:13:05 +0330

    Web server example implemented

--- asm/sockets.c
+++ asm/sockets.c
@@ -1,4 +1,5 @@
 #include <asm-generic/errno-base.h>
+#include <asm-generic/errno.h>
 #include <bits/sockaddr.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,13 +7,11 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-struct sockaddr_in server_addr, cli;
-
 int main(void) {
     // printf("AF_INET = %d\n",AF_INET);
     // printf("SOCK_STREAM = %d\n",SOCK_STREAM);
     // printf("INADDR_ANY= %d\n",INADDR_ANY);
-    // printf("%X\n", htons(6969));
+    // printf("%X\n", htons(8000));
     // printf("%X\n", inet_addr("127.0.0.1"));
     // printf("size of sockaddr_in: %ld\n",sizeof(server_addr));
     // printf("size of sa_family_t: %ld\n",sizeof(server_addr.sin_family));
@@ -21,23 +20,36 @@ int main(void) {
     // printf("size of sin_zero: %ld\n",sizeof(server_addr.sin_zero));
     // printf("size of s_addr inside in_addr: %ld\n",sizeof(server_addr.sin_addr.s_addr));
     // printf("%d\n",htons(8000));
+
+    // printf("EAGAIN      %d\n", EAGAIN);
+    // printf("EBADF       %d\n", EBADF);
+    // printf("ECONNABORTED %d\n", ECONNABORTED);
+    // printf("EFAULT      %d\n", EFAULT);
+    // printf("EINTR       %d\n", EINTR);
+    // printf("EINVAL      %d\n", EINVAL);
+    // printf("EMFILE      %d\n", EMFILE);
+    // printf("ENFILE      %d\n", ENFILE);
+    // printf("ENOBUFS     %d\n", ENOBUFS);
+    // printf("ENOTSOCK    %d\n", ENOTSOCK);
+    // printf("EOPNOTSUPP  %d\n", EOPNOTSUPP);
+    // printf("EPERM       %d\n", EPERM);
+    // printf("EPROTO      %d\n", EPROTO);
+    // printf("IPPROTO_TCP %d\n", IPPROTO_TCP);
     struct sockaddr_in serveraddr;
     int socket_fd = socket(2, 1, 0);
     if (socket_fd < 0) {
         exit(-1);
     }
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    server_addr.sin_port = htons(8000);
-    char* s = (char*) &server_addr;
-    printf("%d\n\n",EACCES);
-    printf("%X,%X\n",s[0],s[1]);
     // **
-    char addr[16] = {0};
+    char addr[16];
     addr[0] = 2;
     addr[1] = 0;
-    addr[2] = 0x39;
-    addr[3] = 0x1b;
+    addr[2] = 0x1b;
+    addr[3] = 0x39;
+    addr[4] = 0;
+    addr[5] = 0;
+    addr[6] = 0;
+    addr[7] = 0;
     if(bind(socket_fd, (struct sockaddr*) &addr, 16) < 0) {
         perror("Bind Failed");
         exit(45);
@@ -46,10 +58,13 @@ int main(void) {
         printf("Listen failed...\n"); 
         exit(0); 
     }
-    socklen_t len = sizeof(cli);
-    int connfd = accept(socket_fd, (struct sockaddr*)&cli, &len); 
+    char cli[16];
+    int a[1] = {0}; 
+    a[0] = 16;
+    printf("%d\n",a[0]);
+    int connfd = accept(socket_fd, (struct sockaddr*)0, (socklen_t*)0); 
     if (connfd < 0) { 
-        printf("server accept failed...\n"); 
+        perror("Accept");
         exit(0); 
     }
     printf("Success!\n");
