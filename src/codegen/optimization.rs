e710b04f59d56d53f0521dd21f29fafd48d05b6f
mahanfarzaneh2000 <mahan.farzaneh@gmail.com>
2024-02-25 21:25:10 +0330

    Use ExprOp as a buffer to codegen

--- src/codegen/optimization.rs
+++ src/codegen/optimization.rs
@@ -1,22 +1,49 @@
+use std::fmt::Display;
+
 use crate::{
     error_handeling::CompilationError,
-    parser::{expr::Op, types::VariableType},
+    parser::{expr::Op, types::VariableType}, compiler::CompilerContext,
 };
 
-use super::instructions::Opr;
+use super::{instructions::{Opr, Instr}, mnemonic::Mnemonic};
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct ExprOpr {
     pub value: Opr,
     pub vtype: VariableType,
+    pub instrs: Vec<Instr>
 }
 
 impl ExprOpr {
     pub fn new(value: impl Into<Opr>, vtype: VariableType) -> Self {
         Self {
             value: value.into(),
+            instrs: Vec::new(),
             vtype,
         }
+    }
+
+    pub fn instr2(&mut self, mnemonic: Mnemonic, opr1: impl Into<Opr>, opr2: impl Into<Opr>) {
+        self.instrs
+            .push(Instr::new2(mnemonic, opr1, opr2));
+    }
+
+    pub fn instr1(&mut self, mnemonic: Mnemonic, opr1: impl Into<Opr>) {
+        self.instrs
+            .push(Instr::new1(mnemonic, opr1));
+    }
+
+    pub fn instr0(&mut self, mnemonic: Mnemonic) {
+        self.instrs.push(Instr::new0(mnemonic));
+    }
+
+    pub fn new_instr(&mut self, instr: Instr) {
+        self.instrs.push(instr);
+    }
+
+    pub fn set_lable(&mut self, lable: impl Display) {
+        let lable = lable.to_string();
+        self.instrs.push(Instr::new1(Mnemonic::Lable, Opr::Loc(lable)));
     }
 }
 
